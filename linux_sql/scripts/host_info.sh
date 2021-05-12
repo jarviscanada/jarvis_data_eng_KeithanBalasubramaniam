5176dc3df815346105d69d0e5c3c1f604f75dc46
Keithan Balasubramaniam <keithan619@gmail.com>
2021-05-12 21:20:11 +0000

    fixed implementation bugs on both scripts

--- linux_sql/scripts/host_info.sh
+++ linux_sql/scripts/host_info.sh
@@ -16,8 +16,8 @@ psql_user=$4
 psql_pass=$5
 
 # Function To Pull Hardware Information
-lscpu_out='lscpu'
-total_mem_out='cat /proc/meminfo'
+lscpu_out=`lscpu`
+total_mem_out=`cat /proc/meminfo`
 
 # Retrieving Hardware Information
 hostname=$(hostname -f)
@@ -25,13 +25,13 @@ cpu_number=$(echo "$lscpu_out"  | egrep "^CPU\(s\):" | awk '{print $2}' | xargs)
 cpu_architecture=$(echo "$lscpu_out"  | egrep "^Architecture:" | awk '{print $2}' | xargs)
 cpu_model=$(echo "$lscpu_out"  | egrep "^Model:" | awk '{print $2}' | xargs)
 cpu_mhz=$(echo "$lscpu_out"  | egrep "^CPU MHz:" | awk '{print $3}' | xargs)
-l2_cache=$(echo "$lscpu_out"  | egrep "^L2 cache:" | awk '{print $3}' | xargs)
-total_mem=$(cat "$total_mem_out" | grep -E "^MemTotal:" | awk '{print $2}' | xargs)
-timestamp=$(data "+%Y-%m-%d %T")
+l2_cache=$(echo "$lscpu_out"  | egrep "^L2 cache:" | awk '{$3=substr($3,1,length($3)-1); print $3}' | xargs)
+total_mem=$(echo "$total_mem_out" | egrep "^MemTotal:" | awk '{print $2}' | xargs)
+timestamp=$(date '+%Y-%m-%d %T')
 
 # Creating Insert Statement For PSQL Database
 insert_stmt="INSERT INTO host_info (hostname, cpu_number, cpu_architecture, cpu_model, cpu_mhz, l2_cache, total_mem, timestamp)
-VALUES ($hostname, $cpu_number, $cpu_architecture, $cpu_model, $cpu_mhz, $l2_cache, $total_mem, $timestamp)"
+VALUES ('$hostname', '$cpu_number', '$cpu_architecture', '$cpu_model', '$cpu_mhz', '$l2_cache', '$total_mem', '$timestamp')"
 
 # Exporting PSQL Password
 export PGPASSWORD=$psql_pass
--- linux_sql/scripts/host_usage.sh
+++ linux_sql/scripts/host_usage.sh
@@ -22,14 +22,15 @@ vmd=$(vmstat -d)
 # Retrieve Resource Information
 timestamp=$(date "+%Y-%m-%d %T")
 hostname=$(hostname -f)
-memory_free=$(echo "$vmt" |tail -1 | awk '{print $4}' | xargs)
+memory_free=$(echo "$(vmstat --unit=M)" |tail -1 | awk '{print $4}' | xargs)
 cpu_idle=$(echo "$vmt" | tail -1 | awk '{print $15}' | xargs)
 cpu_kernel=$(echo "$vmt" | tail -1 | awk '{print $14}' | xargs)
 disk_io=$(echo "$vmd" | tail -1 | awk '{print $10}' | xargs)
-disk_available=$(df -BM / | tail -1 | awk '{print $4}' | sed "s/M//" | xargs)
+disk_available=$(echo "$(df -BM ~)" | tail -1 | awk '{$4=substr($4,1,length($4)-1); print $4}' | xargs)
 
 # Create Insert Statement For PSQL Database
-insert_stmt="INSERT INTO host_usage () VALUES ()"
+insert_stmt="INSERT INTO host_usage (timestamp, host_id, memory_free, cpu_idle, cpu_kernel, disk_io, disk_available)
+VALUES ('$timestamp', (SELECT id FROM host_info WHERE host_info.hostname = '$hostname'), '$memory_free', '$cpu_idle', '$cpu_kernel', '$disk_io', '$disk_available')"
 
 # Exporting PSQL Password
 export PGPASSWORD=$psql_pass
