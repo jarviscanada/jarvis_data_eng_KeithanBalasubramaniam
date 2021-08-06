50468f333fbd226b1c2107f7c1f9ce1657959cb3
Keithan  Balasubramaniam <keithan619@gmail.com>
2021-08-06 17:09:27 -0400

    Set up Python

---
+++ python_data_analytics/psql/data_explore.sql
@@ -0,0 +1,12 @@
+-- Show table schema 
+\d+ retail;
+
+-- Show first 10 rows
+SELECT * FROM retail limit 10;
+
+-- Check # of records
+YOUR QUERY GOES HERE
+
+-- number of clients (e.g. unique client ID)
+YOUR QUERY GOES HERE
+
